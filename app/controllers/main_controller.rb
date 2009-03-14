82749e3f0d9a31254f63fa1764daa4ca7479f0b8
Vicente <vicente.mundim@gmail.com>
2009-03-14 19:27:17 -0300

    Adding an about dialog

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,11 @@
 class MainController < RuGUI::BaseMainController
   def setup_views
     register_view :main_view
+    register_view :about_view
+  end
+
+  def open_about_dialog(widget)
+    self.about_view.about_dialog.show
   end
 
   def quit_application(widget = nil, event = nil)
---
+++ app/views/helpers/about_view_helper.rb
@@ -0,0 +1,2 @@
+class AboutViewHelper < ApplicationViewHelper
+end
